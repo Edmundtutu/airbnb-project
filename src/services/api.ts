b0533e38ccca922bcc679c4cecc5f2c05e50cd5a
Edmund Tutuma <2021bse180@std.must.ac.ug>
2025-09-21 16:24:56 +0300

    Updated api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { AxiosRequestHeaders } from 'axios';
 const ngrokUrl:string = "https://6d98a979a6b0.ngrok-free.app" 
 const api = axios.create({
-  baseURL:  import.meta.env.VITE_API_BASE_URL || ngrokUrl, 
+  baseURL: ngrokUrl, // import.meta.env.VITE_API_BASE_URL ||
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
